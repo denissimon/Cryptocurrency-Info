45954dbf9b5dbc0b5b40a63d0c350317ed0269dc
Denis Simon <denis.v.simon@gmail.com>
2021-02-14 23:29:02 +0300

    Update Constants.swift

--- Cryptocurrency Info/CryptocurrencyInfo/Common/Constants.swift
+++ Cryptocurrency Info/CryptocurrencyInfo/Common/Constants.swift
@@ -11,7 +11,7 @@ struct Constants {
     
     struct ProductionServer {
         static let baseURL = "https://data.messari.io/api"
-        static let limitOnPage = 30
+        static let limitOnPage = 50
         static let messariApiKey = ""._3._1.e._7._2.a._2.e.dash.a._4.f.b.dash._4._6._6.a.dash._9._4._2._7.dash.e._1.f._0._4._8.f._7._2._4._7._4 // 31e72a2e-a4fb-466a-9427-e1f048f72474
     }
     
