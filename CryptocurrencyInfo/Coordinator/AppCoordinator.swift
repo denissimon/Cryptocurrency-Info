ec9441861c335862054f45c9ddd354814b12b31d
Denis <denis.v.simon@gmail.com>
2024-02-05 00:20:28 +0500

    Update AppCoordinator.swift

--- CryptocurrencyInfo/Coordinator/AppCoordinator.swift
+++ CryptocurrencyInfo/Coordinator/AppCoordinator.swift
@@ -7,7 +7,6 @@
 
 import UIKit
 
-// Handles the app's flow
 class AppCoordinator: FlowCoordinator {
     
     // MARK: - Properties
@@ -21,7 +20,6 @@ class AppCoordinator: FlowCoordinator {
     
     // MARK: - Methods
     func start(completionHandler: CoordinatorStartCompletionHandler?) {
-        // We can use MainCoordinator as a coordinator for the entire app, as well as for a first tab (with a separate UINavigationController each) or even just for an app feature such as AssetsFeature (and in this case it's better to rename MainCoordinator to the AssetsFeatureCoordinator, and to move it to the AssetsFeature folder)
         let mainCoordinator = dependencyContainer.makeMainCoordinator(navigationController: navigationController)
         mainCoordinator.start(completionHandler: nil)
     }
