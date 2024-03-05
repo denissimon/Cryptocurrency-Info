de34ff7d0b4de72e83cad5a99a1a2a6fecae6f01
Denis <denis.v.simon@gmail.com>
2024-03-05 05:14:10 +0500

    Updated MessariAPI

--- CryptocurrencyInfo/Common/AppConfiguration.swift
+++ CryptocurrencyInfo/Common/AppConfiguration.swift
@@ -11,7 +11,8 @@ struct AppConfiguration {
     
     struct ProductionServer {
         static let messariBaseURL = "https://data.messari.io/api"
-        static let messariApiKey = ""._3._1.e._7._2.a._2.e.dash.a._4.f.b.dash._4._6._6.a.dash._9._4._2._7.dash.e._1.f._0._4._8.f._7._2._4._7._4 // 31e72a2e-a4fb-466a-9427-e1f048f72474
+        static let messariApiKey = "".J.r.v.c.K.e.Q.O.s.B._6._2.y._9.I.D.O.J.q.C.j.j.R.g.G.h.q.A.w.plus.f.P.r.G.I.Q.Q.u._7.z.L.i._2.X.x.o._3.Z
+ // JrvcKeQOsB62y9IDOJqCjjRgGhqAw+fPrGIQQu7zLi2Xxo3Z
         static let limitOnPage = 50
     }
     
