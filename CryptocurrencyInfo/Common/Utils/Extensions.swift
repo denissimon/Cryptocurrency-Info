90593a71458ac3c5ee314e00a042c76ddc429a89
Denis <denis.v.simon@gmail.com>
2025-04-13 22:58:59 +0300

    Update Extensions.swift

--- CryptocurrencyInfo/Common/Utils/Extensions.swift
+++ CryptocurrencyInfo/Common/Utils/Extensions.swift
@@ -7,9 +7,16 @@
 
 import Foundation
 
-extension Double {
-    func round(to places: Int) -> Double {
-        let divisor = pow(10.0, Double(places))
-        return (self * divisor).rounded() / divisor
+extension Decimal {
+    mutating func round(_ scale: Int, _ roundingMode: NSDecimalNumber.RoundingMode) {
+        var localCopy = self
+        NSDecimalRound(&self, &localCopy, scale, roundingMode)
+    }
+
+    func rounded(_ scale: Int, _ roundingMode: NSDecimalNumber.RoundingMode) -> Decimal {
+        var result = Decimal()
+        var localCopy = self
+        NSDecimalRound(&result, &localCopy, scale, roundingMode)
+        return result
     }
 }
