cc2f7e239db4ddb01259d94666257cbe58e80924
Denis <denis.v.simon@gmail.com>
2025-04-15 04:41:53 +0300

    Update Supportive.swift

--- CryptocurrencyInfo/Common/Utils/Supportive.swift
+++ CryptocurrencyInfo/Common/Utils/Supportive.swift
@@ -9,14 +9,14 @@ import Foundation
 
 class Supportive {
     
-    static func getPriceStr(_ price: Double, currency: PriceCurrency) -> String {
+    static func getPriceStr(_ price: Decimal, currency: PriceCurrency) -> String {        
+        let separator = [.AUD, .CAD, .CLP, .CNH, .CNY, .GBP, .HKD, .IDR, .JPY, .KRW, .MXN, .MYR, .NZD, .PHP, .SGD, .THB, .TRY, .TWD, .USD].contains(currency) ? "" : " "
+        let currecnyArr: [PriceCurrency] = [.ARS, .AUD, .BRL, .CAD, .CHF, .CLP, .CNH, .CNY, .COP, .GBP, .HKD, .IDR, .ILS, .INR, .JPY, .KRW, .MXN, .MYR, .NZD, .PEN, .PHP, .SGD, .THB, .TRY, .TWD, .ZAR, .USD]
         
-        let separator = currency.rawValue.count == 1 ? "" : " "
-        
-        if price < 1.0 {
-            return currency.rawValue + separator + String(format: "%.4f", price)
+        if currecnyArr.contains(currency) {
+            return currency.symbol + separator + String(format: price < 1.0 ? "%.4f" : "%.2f", Double(price.description) ?? Double.zero)
         } else {
-            return currency.rawValue + separator +  String(format: "%.2f", price)
+            return String(format: price < 1.0 ? "%.4f" : "%.2f", Double(price.description) ?? Double.zero) + " " + currency.symbol
         }
     }
 }
