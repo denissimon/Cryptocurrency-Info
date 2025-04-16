564bb216fa6c2745c75bf8a36c517863bdc3134b
Denis <denis.v.simon@gmail.com>
2025-04-16 03:08:04 +0300

    Added CurrencyConversionAPI.swift

---
+++ CryptocurrencyInfo/Data/Network/APIs/CurrencyConversionAPI.swift
@@ -0,0 +1,13 @@
+//
+//  CurrencyConversionAPI.swift
+//  CryptocurrencyInfo
+//
+//  Created by Denis Simon on 16.04.2025.
+//
+
+import Foundation
+import URLSessionAdapter
+
+struct CurrencyConversionAPI {
+    // TODO: Implement using some currency conversion API
+}
