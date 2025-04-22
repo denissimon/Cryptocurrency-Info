32ffb3f459ee287a33f04dea3028d279a2b6a87d
Denis <denis.v.simon@gmail.com>
2025-04-22 10:42:06 +0300

    Changed dependency manager (CocoaPods -> SwiftPM)

--- Pods/Target Support Files/Pods-CryptocurrencyInfo/Pods-CryptocurrencyInfo-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_CryptocurrencyInfo : NSObject
-@end
-@implementation PodsDummy_Pods_CryptocurrencyInfo
-@end
--- Pods/Target Support Files/URLSessionAdapter/URLSessionAdapter-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_URLSessionAdapter : NSObject
-@end
-@implementation PodsDummy_URLSessionAdapter
-@end
