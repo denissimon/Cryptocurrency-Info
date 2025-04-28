f0e7ee631cab8a2212baf29e18f7e1d4dfec978e
Denis <denis.v.simon@gmail.com>
2025-04-28 06:35:54 +0300

    Updated SwiftDataAdapter to be conforming to ModelActor

--- CryptocurrencyInfo/Data/Persistence/SwiftData/SwiftDataAdapter.swift
+++ CryptocurrencyInfo/Data/Persistence/SwiftData/SwiftDataAdapter.swift
@@ -10,40 +10,34 @@
 import Foundation
 import SwiftData
 
-actor SwiftDataAdapter {
+actor SwiftDataAdapter: ModelActor {
     
-    private let container: ModelContainer
-    private var context: ModelContext!
+    nonisolated let modelContainer: ModelContainer
+    nonisolated let modelExecutor: any ModelExecutor
     
-    init(container: ModelContainer) {
-        self.container = container
-    }
+    private var context: ModelContext
     
-    /// Since ModelContexts are not Sendable, this context should be instantiated on the queue from which it is used, not the main.
-    private func createContext(autosaveEnabled: Bool = true) -> ModelContext {
-        let context = ModelContext(container)
+    init(modelContainer: ModelContainer, autosaveEnabled: Bool = true) {
+        context = ModelContext(modelContainer)
         context.autosaveEnabled = autosaveEnabled
-        self.context = context
+        self.modelExecutor = DefaultSerialModelExecutor(modelContext: context)
+        self.modelContainer = modelContainer
         print(context.sqliteUrlPath)
-        return context
     }
     
     /// Insert an object
     func insert<T: PersistentModel>(_ object: T) throws {
-        if context == nil { context = createContext() }
         context.insert(object)
         try context.save()
     }
     
     /// Fetch one or several objects using a descriptor
     func fetch<T: PersistentModel>(_ type: T.Type, descriptor: FetchDescriptor<T>) throws -> [T] {
-        if context == nil { context = createContext() }
         return try context.fetch(descriptor)
     }
     
     /// Delete an object
     func delete<T: PersistentModel>(_ object: T) throws {
-        if context == nil { context = createContext() }
         context.delete(object)
         try context.save()
     }
@@ -51,21 +45,18 @@ actor SwiftDataAdapter {
     /// Delete one or several objects using a predicate.
     /// If you don’t provide a predicate, the context will remove all objects of the specified type.
     func delete<T: PersistentModel>(_ type: T.Type, predicate: Predicate<T>? = nil) throws {
-        if context == nil { context = createContext() }
         try context.delete(model: type, where: predicate)
         try context.save()
     }
     
     /// Delete all objects
     func deleteAll() throws {
-        if context == nil { context = createContext() }
         context.container.deleteAllData() // erase() from iOS 18
         try context.save()
     }
     
     /// Get the number of objects using a descriptor
     func fetchCount<T: PersistentModel>(_ type: T.Type, descriptor: FetchDescriptor<T>) -> Int {
-        if context == nil { context = createContext() }
         let count = (try? context.fetchCount(descriptor)) ?? 0
         return count
     }
